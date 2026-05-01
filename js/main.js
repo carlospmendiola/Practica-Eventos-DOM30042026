499b18c57bc7dda6e3abebfc829dafc8c0c0b374
carlospmendiola <carlos@carlospmendiola.com>
2026-05-01 18:03:51 +0200

    Subimos el html y el js modificado y las imágenes del proyecto

--- js/main.js
+++ js/main.js
@@ -1,1 +1,66 @@
-console.log("hola")
+const arrayBotones = [
+  
+]
+
+const arrayImagenes = [
+  {
+    id: imagen1,
+    alt: 'Alt-imagen1',
+    tags: ['cielo','campo','globos']
+  },
+  {
+    id: imagen2,
+    alt: 'Alt-imagen2',
+    tags: ['cielo','mar','sol']
+  },
+  {
+    id: imagen3,
+    alt: 'Alt-imagen3',
+    tags: ['cielo','nubes','sol']
+  },
+  {
+    id: imagen4,
+    alt: 'Alt-imagen4',
+    tags: ['cielo','nubes','sol']
+  },
+  {
+    id: imagen5,
+    alt: 'Alt-imagen5',
+    tags: ['cielo','ciudades']
+  },
+  {
+    id: imagen6,
+    alt: 'Alt-imagen6',
+    tags: ['edificio','rio']
+  },
+  {
+    id: imagen7,
+    alt: 'Alt-imagen7',
+    tags: ['edificios','monte']
+  },
+  {
+    id: imagen8,
+    alt: 'Alt-imagen8',
+    tags: ['edificio','rio']
+  },
+  {
+    id: imagen9,
+    alt: 'Alt-imagen9',
+    tags: ['mar','pajaro','sol']
+  },
+  {
+    id: imagen10,
+    alt: 'Alt-imagen10',
+    tags: ['cielo','nubes','globos']
+  },
+  {
+    id: imagen11,
+    alt: 'Alt-imagen11',
+    tags: ['playa','mar','edificio']
+  },
+  {
+    id: imagen12,
+    alt: 'Alt-imagen12',
+    tags: ['playa','mar','chica']
+  }
+]
