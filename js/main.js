4d60cf89a6ca9cd4e294e36efa5051458c7d2e85
carlospmendiola <carlos@carlospmendiola.com>
2026-05-03 14:36:22 +0200

    Subimos los archivos a la nueva rama creada con nuevas modificaciones realizadas

--- js/main.js
+++ js/main.js
@@ -8,46 +8,54 @@ const arrayImagenes = [
   {
     imagen: 'images/img1-cielo-campo-globos.jpg', 
     id: 'imagen1',
+    titulo: 'Cielos y globos',
     alt: 'Alt-imagen1',
     tags: ['nubes','globos']
   },
   {
     imagen: 'images/img2-cielo-mar-sol.jpg', 
     id: 'imagen2',
+    titulo: 'Atardecer en el mar',
     alt: 'Alt-imagen2',
     tags: ['nubes','mar','sol']
   },
   {
     imagen: 'images/img3-cielo-nubes-sol.jpg', 
     id: 'imagen3',
+    titulo: 'Nubes al amanecer',
     alt: 'Alt-imagen3',
     tags: ['cielo','sol']
   },
   {
     imagen: 'images/img4-cielo-sol.jpg', 
+    titulo: 'La luna tras las nubes',
     id: 'imagen4',
     alt: 'Alt-imagen4',
     tags: ['nubes','sol']
   },
   { imagen: 'images/img5-nubes-globos.jpg',
+    titulo: 'Globos en el cielo',
     id: 'imagen5',
     alt: 'Alt-imagen5',
     tags: ['nubes','globos']
   },
   {
     imagen: 'images/img6-edificio-rio.jpg',
+    titulo: 'Edificio de Sevilla',
     id: 'imagen6',
     alt: 'Alt-imagen6',
     tags: ['edificio','cielo']
   },
   {
     imagen: 'images/img7-edificio-piedra.jpg',
+    titulo: 'Pueblo bajo las murallas',
     id: 'imagen7',
     alt: 'Alt-imagen7',
     tags: ['edificio','cielo']
   },
   {
     imagen: 'images/img8-playa-mar.jpg',
+    titulo: 'Casita en el mar',
     id: 'imagen8',
     alt: 'Alt-imagen8',
     tags: ['mar','cielo','nubes']
@@ -75,27 +83,60 @@ const arrayImagenes = [
       const imagenesFiltradas = arrayImagenes.filter((imagen) => {
        return imagen.tags.includes(tag);
       });
+      /* Cogemos el título de cada imagen del array para crear el h3 */
+      const crearTitulo = document.createElement('h3');
+      crearTitulo.textContent = (imagenesFiltradas[0].titulo);
+
+      crearTitulo.classList.add('colorPrincipal');
+      crearTitulo.classList.add('mayusculas');
       /* Filtramos y pintamos la imagen central pero solo la primera y le añadimos el alt a esta primara imagen */
       const crearImagencentral = document.createElement('img');
       crearImagencentral.src = imagenesFiltradas[0].imagen;
       crearImagencentral.alt = imagenesFiltradas[0].alt;
 
+
       /* Añadimos esto para evitar que cada vez que hacemos click en los botones no se repita ni el texto de presentación ni las imágenes, con esto limpiamos antes de hacer la llamada a la imagen y al texto  */  
+
       txtPresentacion.innerHTML = '';
       imagenPrincipal.innerHTML = '';
       imgSecundarias.innerHTML = '';
 
       /* Ahora filtramos las restantes imágenes con el alt */
       imagenesFiltradas.slice(1).forEach ((imagen) => {
+        /* Creamos el div donde irá cada imagen */
         const crearDivImgSec = document.createElement('div')
+        /* Creamos el h4 del título de cada imagen del array */
+        const crearTituloh4 = document.createElement('h4');
+        /* Creamos el elemento imagen del array para crearla imagen secundaria */
+        
         const crearImgsecundarias = document.createElement('img');
 
-        
+        crearTituloh4.textContent = (imagen.titulo);
+        /* Función para crear el cambio de imagen princiàl y secundaria */
+        crearImgsecundarias.addEventListener("click", () => {
+          /* Guardo el src actual de la imagen principal en una variable temporal */
+          const valorImagen = crearImagencentral.src;
+          /* Asignamos el src de la imagen secundaria a la imagen principal */
+          crearImagencentral.src = crearImgsecundarias.src;
+          /* Hace el intercambio de la imagen secundaria con la principal */
+          crearImgsecundarias.src = valorImagen;
+
+          /* Guardo el titulo actual de la imagen principal en una variable temporal */
+          const valorTitulo = crearTitulo.textContent;
+          /* Asignamos el titulo de la imagen secundaria a la imagen principal */
+          crearTitulo.textContent = crearTituloh4.textContent;
+          /* Hace el intercambio del titulo de la imagen secundaria a la principal */
+          crearTituloh4.textContent = valorTitulo;
+        });
+
         crearImgsecundarias.src = imagen.imagen;
         crearImgsecundarias.alt = imagen.alt;
 
-        crearDivImgSec.append(crearImgsecundarias)
-        imgSecundarias.append(crearDivImgSec)
+        
+        crearDivImgSec.append(crearImgsecundarias);
+        crearDivImgSec.append(crearTituloh4)
+        imgSecundarias.append(crearDivImgSec);
+        // imgSecundarias.append(crearTituloh4);
 
       });
 
@@ -105,7 +146,9 @@ const arrayImagenes = [
 
 
       txtPresentacion.append(crearDescripcion);
+      imagenPrincipal.append(crearTitulo);
       imagenPrincipal.append(crearImagencentral);
+      
     
     });
     fragment.append(crearBoton);
